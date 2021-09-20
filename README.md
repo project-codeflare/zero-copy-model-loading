0546c667839ae8951692c884b7abe60b45859411
frreiss <frreiss@us.ibm.com>
2021-09-20 15:25:18 -0700

    Initial import of first notebook

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # zero-copy-model-loading
+
+Code to accompany my Medium post, ["How to Load PyTorch Models 340 Times Faster
+with
+Ray"](https://medium.com/ibm-data-ai/how-to-load-pytorch-models-340-times-faster-with-ray-8be751a6944c).
+
+Notebooks:
+* `zero_copy_loading.ipynb`: The notebook that was used when authoring the original blog post.
+
+Instructions to run:
+1. Install `bash` and either `anaconda` or `miniconda`.
+1. Check out a copy of this repository and navigate to the root directory of
+   your local copy.
+1. Run the script `./env.sh`, which creates an Anaconda environment under 
+   `<root of your local copy>/env`.
+   ```
+   ./env.sh
+   ```
+1. Activate the Anaconda environment:
+   ```
+   conda activate ./env
+   ```
+1. Run JupyterLab:
+   ```
+   jupyter lab
+   ```
+1. Open up the Jupyter notebook of your choice.
+
+
---
+++ env.sh
@@ -0,0 +1,90 @@
+#! /bin/bash
+
+
+# Create conda environment to run the notebooks in this directory.
+#
+# By default, the environment will be located in the directory "env"
+# immediately under this one. To override that setting,
+# pass the subdirectory name as the first argument to this script, i.e.
+#
+# $ ./env.sh my_dir_name
+
+PYTHON_VERSION=3.8
+
+############################
+# HACK ALERT *** HACK ALERT
+# The friendly folks at Anaconda thought it would be a good idea to make the
+# "conda" command a shell function.
+# See https://github.com/conda/conda/issues/7126
+# The following workaround will probably be fragile.
+if [ -z "$CONDA_HOME" ]
+then
+    echo "Error: CONDA_HOME not set."
+    exit
+fi
+if [ -e "${CONDA_HOME}/etc/profile.d/conda.sh" ]
+then
+    # shellcheck disable=SC1090
+    . "${CONDA_HOME}/etc/profile.d/conda.sh"
+else
+    echo "Error: CONDA_HOME (${CONDA_HOME}) does not appear to be set up."
+    exit
+fi
+# END HACK
+############################
+
+# Check whether the user specified an environment name.
+if [ "$1" != "" ]; then
+    ENV_DIR=$1
+else
+    ENV_DIR="env"
+fi
+echo "Creating an Anaconda environment at ./${ENV_DIR}"
+
+
+# Remove the detrius of any previous runs of this script
+rm -rf ./${ENV_DIR}
+
+# Note how we explicitly install pip on the line that follows. THIS IS VERY
+# IMPORTANT!
+conda create -y -p ${ENV_DIR} python=${PYTHON_VERSION} pip
+conda activate ./${ENV_DIR}
+
+################################################################################
+# Install packages with conda
+
+# We currently install JupyterLab from conda because the pip packages are 
+# broken for Anaconda environments with Python 3.6 and 3.8 on Mac, as of
+# April 2021.
+conda install -y -c conda-forge jupyterlab
+conda install -y -c conda-forge/label/main nodejs
+conda install -y -c conda-forge jupyterlab-git
+
+################################################################################
+# Install packages with pip
+
+# Pip dependencies are 
+pip install -r requirements.txt
+
+################################################################################
+# Custom install steps
+
+# Elyra extensions to JupyterLab (enables git integration, debugger, workflow
+# editor, outlines, and other features)
+pip install --upgrade --use-deprecated=legacy-resolver elyra
+
+# Rebuild JupyterLab environment 
+jupyter lab build
+
+jupyter --version
+echo " "
+jupyter serverextension list
+echo " "
+jupyter labextension list
+echo " "
+
+conda deactivate
+
+echo "Anaconda environment at ./${ENV_DIR} successfully created."
+echo "To use, type 'conda activate ./${ENV_DIR}'."
+
